b92117a22f5cf23d2f28abd0c6f3d9fb83fa1adc
Nikita Mediankin <mediankin@ibis.ms.mff.cuni.cz>
2016-11-09 16:42:49 +0100

    Tree traversing

--- derinet.py
+++ derinet.py
@@ -30,7 +30,7 @@ class DeriNet:
         with open(fname, 'r', encoding='utf-8') as ifile:
             for i, line in enumerate(ifile):
                 lex_id, lemma, deriv, pos, parent = line.strip('\n').split('\t')
-                self.data.append(Lexeme(lex_id, lemma, deriv, pos, int(parent) if parent !='' else '', []))
+                self.data.append(Lexeme(int(lex_id), lemma, deriv, pos, int(parent) if parent !='' else '', []))
                 self.index[lemma].append(int(lex_id))
 
         if len(self.data) != i + 1:
@@ -40,7 +40,7 @@ class DeriNet:
         for lemma, lex_id_list in self.index.items():
             lex_id_list.sort()
 
-        for i, lexeme in enumerate(self.data):
+        for lexeme in self.data:
             if lexeme.parent != '':
                 self.data[lexeme.parent].children.append(lexeme)
 
@@ -60,17 +60,29 @@ class DeriNet:
         return self.data[lex_id]
 
     def get_parent_by_id(self, lex_id):
-        parent = self.data[lex_id].parent
-        if parent is None:
+        parent_id = self.data[lex_id].parent
+        if parent_id is None:
             return None
-        return self.data[parent]
+        return self.data[parent_id]
 
     def get_ids_by_lemma(self, lemma):
         return self.index[lemma]
 
     def get_children_by_id(self, lex_id):
-        pass
+        return self.data[lex_id].children
 
+    def get_subtree_by_id(self, lex_id):
+        lexeme = self.data[lex_id]
+        return [lexeme, [self.get_subtree_by_id(child.id) for child in lexeme.children]]
+
+    def print_subtree_by_id(self, lex_id, form1='', form2='  ', form3=''):
+        lexeme = self.data[lex_id]
+        print(form1 + form3, end='')
+        print(*lexeme[:-1], sep='\t')
+        if lexeme.children != []:
+            for child in lexeme.children[:-1]:
+                self.print_subtree_by_id(child.id, form1=form1+form2, form2='│ ', form3='└─')
+            self.print_subtree_by_id(lexeme.children[-1].id, form1+form2, form2='  ', form3='└─')
 
 if __name__ == "__main__":
     derinet = DeriNet()
